a1b16ed96a1903e11c02cfad9c9d4308ea522c67
mendesjorge <luis.7win@gmail.com>
2013-10-16 18:25:04 +0100

    [CHANGES]newsBar slide effect

--- scripts/newsReader.js
+++ scripts/newsReader.js
@@ -15,8 +15,9 @@ google.load("feeds", "1");
 	});
 });*/
 
-function lineFunc(data){
-	return "<li><a href='"+data.attr("link")+"'>" + data.attr("title")+"</a></li>";
+function lineFunc(data,i){
+	return i>0 ?"<li class='noshow'><a href='"+data.attr("link")+"'>" + data.attr("title")+"</a></li>"
+		:"<li><a href='"+data.attr("link")+"'>" + data.attr("title")+"</a></li>";
 }
 
 
@@ -26,41 +27,47 @@ var newsInitCallBack = function(){
 	feed.load(function (data) {
 	    // Parse data depending on the specified response format, default is JSON.
 	    $(data.feed.entries).each(function(i){
-	    	news.find("ul").append(lineFunc($(this)));
+	    	news.find("ul").append(lineFunc($(this),i));
 	    });
 
 	     console.log(data);
 	});
 }
 
+
+function changeScrolling(el1, el2){
+	el1.slideUp('slow');
+	el2.slideDown('slow');
+}
+
 $(document).ready(function(){
 	news = $($("div#news")[0]);
 	newsInitCallBack();
 
+	var ulElem = news.find("ul");
+    var scrolling,
+    i = 0,
+    speed = 2000;
+    
+    ulElem.mouseleave(function() {
+    	var els = $(ulElem[0]).children();
+		var lastIdx = els.length-1;
 
-	var ele = news.find("ul");
-
-    var speed = 2000,
-        scroll = 20,
-        top = 0,
-        maxTop = 60,
-        scrolling;
-
-    ele.mouseleave(function() {
         scrolling = window.setInterval(function() {
-            top = top >= maxTop ? 0 : ele.scrollTop() + scroll;
-            ele.scrollTop(top);
+            $(els[i]).slideUp('slow');
+            i = i >= lastIdx ? 0 : i+1;
+            $(els[i]).slideDown('slow');
         }, speed);
     });
 
-    ele.mouseenter(function() {
+    ulElem.mouseenter(function() {
         if (scrolling) {
             window.clearInterval(scrolling);
             scrolling = false;
         }
     });
     
-    ele.mouseleave();
+    ulElem.mouseleave();
 
 });
 
